6b076e2d68dfb668764b9d8586c0484107a83b5d
endophasie <albina.tokarchuk@gmail.com>
2017-05-29 17:59:05 +0700

    Add key const

--- js/main.js
+++ js/main.js
@@ -8,9 +8,11 @@ const screens = [
 const mainContainer = document.querySelector(`.main`);
 const tmpl = document.querySelector(`#templates`);
 
-const KEY_BASE = 18;
-const KEY_FORWARD = 39;
-const KEY_BACK = 37;
+const KEY = {
+  BASE: 18,
+  FORWARD: 39,
+  BACK: 37
+}
 
 let activeScreen = 0;
 
@@ -28,11 +30,11 @@ showScreen(activeScreen);
 
 document.addEventListener(`keydown`, function (event) {
   switch (event.keyCode) {
-    case KEY_BASE && KEY_BACK:
+    case KEY.BASE && KEY.BACK:
       activeScreen = Math.max(activeScreen - 1, 0);
       showScreen(activeScreen);
       break;
-    case KEY_BASE && KEY_FORWARD:
+    case KEY.BASE && KEY.FORWARD:
       activeScreen = Math.min(activeScreen + 1, screens.length - 1);
       showScreen(activeScreen);
       break;
